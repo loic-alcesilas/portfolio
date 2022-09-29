5da0986caef41e53607021c86d238eef79c9f9f7
Loic alcesilas <loic.alcesilas@gmail.com>
2022-09-29 21:55:24 +0200

    ajout du service_id et template_id manquant

--- public/js/contact.js
+++ public/js/contact.js
@@ -1,42 +1,42 @@
 // Email JS
 function validate() {
-    let name = document.querySelector(".name");
-    let email = document.querySelector(".email");
-    let msg = document.querySelector(".message");
-    let sendBtn = document.querySelector(".send-btn");
-  
-    sendBtn.addEventListener("click", (e) => {
-      e.preventDefault();
-      if (name.value == "" || email.value == "" || msg.value == "") {
-        emptyerror();
-      } else {
-        sendmail(name.value, email.value, msg.value);
-        success();
-      }
-    });
-  }
-  validate();
-  
-  function sendmail(name, email, msg) {
-    // User Your Service id and template id here
-    emailjs.send("service_id", "template_id", {
-      from_name: email,
-      to_name: name,
-      message: msg,
-    });
-  }
-  
-  function emptyerror() {
-    swal({
-      title: "Oh No....",
-      text: "Certains champs sont vide !",
-      icon: "error",
-    });
-  }
-  function success() {
-    swal({
-      title: "Email envoyé avec succées",
-      text: "Réponse sous 24 heure",
-      icon: "success",
-    });
-  }
+  let name = document.querySelector(".name");
+  let email = document.querySelector(".email");
+  let msg = document.querySelector(".message");
+  let sendBtn = document.querySelector(".send-btn");
+
+  sendBtn.addEventListener("click", (e) => {
+    e.preventDefault();
+    if (name.value == "" || email.value == "" || msg.value == "") {
+      emptyerror();
+    } else {
+      sendmail(name.value, email.value, msg.value);
+      success();
+    }
+  });
+}
+validate();
+
+function sendmail(name, email, msg) {
+  // User Your Service id and template id here
+  emailjs.send("service_5jc52fb", "template_c2k84v7", {
+    from_name: email,
+    to_name: name,
+    message: msg,
+  });
+}
+
+function emptyerror() {
+  swal({
+    title: "Oh No....",
+    text: "Certains champs sont vide !",
+    icon: "error",
+  });
+}
+function success() {
+  swal({
+    title: "Email envoyé avec succées",
+    text: "Réponse sous 24 heure",
+    icon: "success",
+  });
+}
