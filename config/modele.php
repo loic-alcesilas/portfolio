d566563f6ba316862adcfaa205cded8d03ce87a0
Loic alcesilas <loic.alcesilas@gmail.com>
2022-10-03 15:40:20 +0200

    ajout de la connexion a la BDD et des requetes preparer

---
+++ config/modele.php
@@ -0,0 +1,32 @@
+<?php
+
+namespace OpenClassrooms\Portfolio\Modele; // La classe sera dans ce namespace
+
+abstract class Modele
+{
+
+    /** Objet PDO d'accès à la BD */
+    private $bdd;
+
+    protected function executerRequete($sql, $params = null)
+    {
+        if ($params == null) {
+            $resultat = $this->getBdd()->query($sql); // exécution directe
+        } else {
+            $resultat = $this->getBdd()->prepare($sql); // requête préparée
+            $resultat->execute($params);
+        }
+        return $resultat;
+    }
+
+    private function getBdd()
+    {
+        if ($this->bdd == null) {
+            // Création de la connexion
+            $this->bdd = new \PDO('mysql:host=localhost;dbname=portfolio;charset=utf8', 'root', '',
+                array(\PDO::ATTR_ERRMODE => \PDO::ERRMODE_EXCEPTION));
+        }
+        return $this->bdd;
+    }
+
+}
