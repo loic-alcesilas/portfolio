91eaf37f077014c43d92bebc418144abeac47809
Loic alcesilas <loic.alcesilas@gmail.com>
2022-10-06 15:35:20 +0200

    mise en place dun tableau avec tous les projets pour le crud sur la page administration

--- vue/vueAdmin.php
+++ vue/vueAdmin.php
@@ -1,13 +1,21 @@
-<?php $this->titre = "Administration";?>
+<?php $this->titre = "Administration"; ?>
 
 <section class="dashboard">
+    <h2 class="heading">mes projets</h2>
     <table class="allProjet">
         <tr>
-            <td></td>
-            <td></td>
+            <td>TITRE PROJETS</td>
+            <td>  <a href=""><i class='bx bxs-plus-circle' ></i> </a></td>
         </tr>
+    <?php foreach ($projets as $projet) : ?>
+        <tr>
+            <td class="titreadmin"><?= $projet['titre'] ?></td>
+            <td>
+            <a href="<?= "index.php?action=modifier&id" . $projet['id'] ?>"><i id="edit" class='bx bxs-edit' aria-hidden="true"></i></a>
+            <a href="<?= "index.php?action=delete&id" . $projet['id'] ?>"><i id="del" class='bx bxs-x-square' aria-hidden="true"></i> </a>
+        </td>
+        </tr>
+    <?php endforeach; ?>
     </table>
-    <?php foreach ($projets as $projet): ?>
-        
-        <?php endforeach;?>
+    
 </section>
